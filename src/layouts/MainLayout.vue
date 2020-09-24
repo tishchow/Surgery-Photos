fb5d7dd7bd42e204f819e6366965025f4d894c90
tishchow <62623893+tishchow@users.noreply.github.com>
2020-09-23 21:40:24 -0700

    distribution build

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title>
-          Tish's Mommy Makeover
-        </q-toolbar-title>
+        <q-toolbar-title> Tish's Mommy Makeover </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
@@ -22,44 +20,44 @@ const linksData = [
     title: "Docs",
     caption: "quasar.dev",
     icon: "school",
-    link: "https://quasar.dev"
+    link: "https://quasar.dev",
   },
   {
     title: "Github",
     caption: "github.com/quasarframework",
     icon: "code",
-    link: "https://github.com/quasarframework"
+    link: "https://github.com/quasarframework",
   },
   {
     title: "Discord Chat Channel",
     caption: "chat.quasar.dev",
     icon: "chat",
-    link: "https://chat.quasar.dev"
+    link: "https://chat.quasar.dev",
   },
   {
     title: "Forum",
     caption: "forum.quasar.dev",
     icon: "record_voice_over",
-    link: "https://forum.quasar.dev"
+    link: "https://forum.quasar.dev",
   },
   {
     title: "Twitter",
     caption: "@quasarframework",
     icon: "rss_feed",
-    link: "https://twitter.quasar.dev"
+    link: "https://twitter.quasar.dev",
   },
   {
     title: "Facebook",
     caption: "@QuasarFramework",
     icon: "public",
-    link: "https://facebook.quasar.dev"
+    link: "https://facebook.quasar.dev",
   },
   {
     title: "Quasar Awesome",
     caption: "Community Quasar projects",
     icon: "favorite",
-    link: "https://awesome.quasar.dev"
-  }
+    link: "https://awesome.quasar.dev",
+  },
 ];
 
 export default {
@@ -68,8 +66,8 @@ export default {
   data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData
+      essentialLinks: linksData,
     };
-  }
+  },
 };
 </script>
